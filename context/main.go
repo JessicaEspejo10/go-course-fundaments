6ff513851633ce250decbc91fd51dbefc41c4b65
Jessica Espejo <nataespejot@gmail.com>
2024-09-24 18:14:37 -0500

    ejercicio de context en go

---
+++ context/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "my-key", "my-value")
+	ctx = context.WithValue(ctx, "my-key-int", 5)
+	viewcContext(ctx)
+
+	ctx2, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	//select trabaja con canales y funciona como el switch
+
+	go myProcess(ctx2)
+	
+	select {
+	case <-ctx2.Done():
+		fmt.Println("we've exceeded the deadline")
+		fmt.Println(ctx2.Err())
+
+	}
+}
+
+func viewcContext(ctx context.Context) {
+	fmt.Printf("my value is %s \n", ctx.Value("my-key"))
+	fmt.Printf("my other value is %d \n", ctx.Value("my-key-int"))
+
+}
+
+func myProcess(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("ohh, time out")
+			return
+		default:
+			fmt.Println("doing some process")
+		}
+	}
+	time.Sleep(500 * time.Millisecond)
+}
